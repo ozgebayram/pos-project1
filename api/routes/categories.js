5923b06d43c927f5a8b0927d30bcb942f929fdfe
özgebayram <ozgebayram1234@gmail.com>
2023-09-27 23:09:56 +0300

    feat: express router

---
+++ api/routes/categories.js
@@ -0,0 +1,17 @@
+const Category = require("../models/Category.js");
+const express = require("express");
+const router = express.Router();
+
+
+router.post("/add-category", async (req,res)=>{
+	try {
+		const newCategory = new Category(req.body);
+		await newCategory.save();
+		res.status(200).json("Item added succesfully.")
+	} catch (error) {
+		res.status(400).json(error)
+		
+	}
+})
+module.exports =router;
+
--- api/server.js
+++ api/server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const dotenv = require("dotenv")
-
+const dotenv = require("dotenv");
 const app = express();
-
+const cors = require("cors");
 const port = 5000;
-dotenv.config()
+
+//routes
+
+const categoryRoute = require("./routes/categories.js");
+
+dotenv.config();
 
 const connect = async () => {
   try {
@@ -16,7 +20,9 @@ const connect = async () => {
   }
 };
 
-app.get("/", (req, res) => res.send("Hello world!"));
+//middlewares
+app.use(express.json());
+app.use("/api/categories", categoryRoute);
 
 app.listen(port, () => {
   connect();
