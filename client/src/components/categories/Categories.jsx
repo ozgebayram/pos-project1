e038e0ca9975b8bdaa30d1c95bd116cff3f25eeb
özgebayram <ozgebayram1234@gmail.com>
2023-10-08 00:00:34 +0300

    feat: categories crud

--- client/src/components/categories/Categories.jsx
+++ client/src/components/categories/Categories.jsx
@@ -1,82 +1,49 @@
-import { PlusOutlined } from "@ant-design/icons";
+import { PlusOutlined, EditOutlined } from "@ant-design/icons";
 import { useState } from "react";
 import { Button, Form, Input, message, Modal } from "antd";
 import "./style.css";
+import Add from "./Add";
+import Edit from "./Edit";
 
-const Categories = () => {
+const Categories = ({ categories, setCategories }) => {
   const [isAddModalOpen, setIsAddModalOpen] = useState(false);
-  const [form] = Form.useForm();
-
-  const onFinish = (values) => {
-    try {
-      fetch("http://localhost:5000/api/categories/add-category", {
-        method: "POST",
-        body: JSON.stringify(values),
-        headers: { "Content-type": "application/json; charset=UTF-8" },
-      });
-      message.success("Kategori başarıyla eklendi.");
-      form.resetFields();
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
 
   return (
     <ul className="flex gap-4 md:flex-col text-lg">
-      <li className="category-item">
-        <span>Tümü</span>
-      </li>
-      <li className="category-item">
-        <span>Yiyecek</span>
-      </li>
-      <li className="category-item">
-        <span>İçecek</span>
-      </li>
-      <li className="category-item">
-        <span>İçecek</span>
-      </li>
-      <li className="category-item">
-        <span>İçecek</span>
-      </li>
-      <li className="category-item">
-        <span>İçecek</span>
-      </li>
-      <li className="category-item">
-        <span>İçecek</span>
-      </li>
-      <li className="category-item">
-        <span>İçecek</span>
-      </li>
+      {categories.map((item) => (
+        <li
+          className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px] flex items-center justify-center"
+          key={item._id}
+        >
+          <span>{item.title}</span>
+        </li>
+      ))}
 
       <li
-        className="category-item !bg-purple-800 hover:opacity-90"
+        className="px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px] flex items-center justify-center !bg-purple-800 hover:opacity-90"
         onClick={() => setIsAddModalOpen(true)}
       >
         <PlusOutlined className="md:text-2xl" />
       </li>
-      <Modal
-        title="Yeni Kategori Ekle"
-        open={isAddModalOpen}
-        onCancel={() => setIsAddModalOpen(false)}
-        footer={false}
+      <li
+        className="px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px] flex items-center justify-center !bg-orange-800 hover:opacity-90"
+        onClick={() => setIsEditModalOpen(true)}
       >
-        <Form layout="vertical" onFinish={onFinish} form={form}>
-          <Form.Item
-            name="title"
-            label="Kategori Ekle"
-            rules={[
-              { required: true, message: "Kategori Alanı Boş Geçilemez!" },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item className="flex justify-end mb-0">
-            <Button type="primary" htmlType="submit">
-              Oluştur
-            </Button>
-          </Form.Item>
-        </Form>
-      </Modal>
+        <EditOutlined className="md:text-2xl" />
+      </li>
+      <Add
+        setIsAddModalOpen={setIsAddModalOpen}
+        isAddModalOpen={isAddModalOpen}
+        categories={categories}
+        setCategories={setCategories}
+      />
+      <Edit
+        isEditModalOpen={isEditModalOpen}
+        setIsEditModalOpen={setIsEditModalOpen}
+        categories={categories}
+        setCategories={setCategories}
+      />
     </ul>
   );
 };
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,32 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import CartTotals from "../components/cart/CartTotals";
 import Categories from "../components/categories/Categories";
 import Header from "../components/header/Header";
 import Products from "../components/products/Products";
 
 const HomePage = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const getCategories = async () => {
+      try {
+        const res = await fetch("http://localhost:5000/api/categories/get-all");
+        const data = await res.json();
+        setCategories(data)
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getCategories()
+  }, []);
+  console.log(categories);
+
   return (
-	<>
-	<Header/>
+    <>
+      <Header />
       <div className="home px-6 flex md:flex-row flex-col justify-between gap-10 md:pb-0 pb-24">
         <div className="categories  overflow-auto max-h-[calc(100vh_-_112px)] md:pb-10">
           {/* yukarıda scroll verdik ve pb-64 ile de scroll un sonunda boşluk bıraktık */}
           {/* bu bir slider kutuphanesi ile de yapılabılır. */}
-          <Categories/>
+          <Categories categories={categories} setCategories={setCategories}/>
         </div>
         <div className="products flex-[8] max-h-[calc(100vh_-_112px)] overflow-y-auto pb-10">
-          <Products/>
+          <Products />
         </div>
         <div className="card-wrapper min-w-[300px] md:-mr-[24px] md:-mt-[24px] border">
-          <CartTotals/>
+          <CartTotals />
         </div>
       </div>
-	  </>
-  )
-}
+    </>
+  );
+};
 // md:-mr-[24px] md:-mt-[24px] = bu özellik shopping list yazısını yukarı ve sola sıfırladı
 
-
-export default HomePage
-
-
+export default HomePage;
